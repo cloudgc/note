4586f5f8bf831d0e9d29c49e11a8874fdc96563a
sunzhongwen <sunzhongwen@fullgoalos.com.cn>
2019-01-21 17:50:09 +0800

    add k8s

--- linux/kubernetes/admin/chapter1.sh
+++ linux/kubernetes/admin/chapter1.sh
@@ -45,6 +45,9 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
+#==============
+kubeadm join
+
 
 kubectl get nodes
 
